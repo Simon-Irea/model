bc5fb744b153c5bbcc7e8de64cc809539a093392
simon <349676014@qq.com>
2019-01-08 23:43:40 +0800

    添加了装饰器模式

---
+++ src/com/decorator/Client.java
@@ -0,0 +1,19 @@
+package com.decorator;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.move();
+		
+		FlyCar flycar = new FlyCar(car);
+		flycar.move();
+		
+		WaterCar watercar = new WaterCar(car);
+		watercar.move();
+		
+		AICar aicar = new AICar(watercar);
+		aicar.move();
+	}
+
+}
