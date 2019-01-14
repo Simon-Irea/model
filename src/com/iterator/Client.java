e182283396845c66ebb5624029c61ae35385b2a1
simon <349676014@qq.com>
2019-01-15 00:03:46 +0800

    1.0.0

---
+++ src/com/iterator/Client.java
@@ -0,0 +1,17 @@
+package com.iterator;
+
+public class Client {
+	public static void main(String[] args) {
+		ConcreteMyAggregate cma = new ConcreteMyAggregate();
+		cma.addObject("aa");
+		cma.addObject("bb");
+		cma.addObject("cc");
+		
+		MyIterator iter = cma.createIterator();
+		while (iter.hasNext()) {
+			//一般在返回一个对象的时候游标会自动加一
+			System.out.println(iter.getCurrentObj());
+			iter.next();
+		}
+	}
+}
